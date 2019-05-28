5216223c344c4d11ab25b524770e5f2320a16592
sylvien <sylvien@vmware.com>
2019-05-28 17:49:12 +0200

    Add WSO App Analytics part

--- src/main/java/com/example/test/MainActivity.java
+++ src/main/java/com/example/test/MainActivity.java
@@ -1,22 +1,36 @@
 package com.example.test;
 
-
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Build;
 import android.view.View;
-import android.graphics.Color;
 import android.widget.Button;
 import android.widget.TextView;
 
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.fragment.app.DialogFragment;
+
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
+import androidx.viewpager.widget.ViewPager;
 
 import com.crittercism.app.Crittercism;
 import com.crittercism.app.CrittercismCallback;
 import com.crittercism.app.CrittercismConfig;
 import com.crittercism.app.CrashData;
-//import com.crittercism.error.CRXMLHttpRequestException;
-//import com.crittercism.app.CritterCallback;
+
+import com.example.test.fragments.ErrorFragment;
+import com.example.test.fragments.NetworkFragment;
+import com.example.test.fragments.OtherFragment;
+import com.example.test.fragments.TransactionFragment;
+
 
 /**
  * Main application object.
@@ -26,14 +40,33 @@ import com.crittercism.app.CrashData;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
         //Initialize Crittercism
         Crittercism.initialize(getApplicationContext(), "61b7fd650b2f4cf58fc72478b16f38f700555300");
 
-        setContentView(R.layout.activity_main);
+        ViewPager viewPager = findViewById(R.id.pager);
+        TabLayout tablayout = findViewById(R.id.tabLayout);
+        tabLayout.setupWithViewPager(viewPager);
+        tabLayout.setTabGravity(TabLayout.GRAVITY_CENTER);
+        tabLayout.setTabMode(TabLayout.MODE_SCROLLABLE);
+
+/*        tablayout.addTab(tablayout.newTab().setText("Crash"));
+        tablayout.addTab(tablayout.newTab().setText("Perf"));
+        tablayout.addTab(tablayout.newTab().setText("Usage"));
+        tablayout.addTab(tablayout.newTab().setText("Network"));*/
+
+        TabItem tabCrash = findViewById(R.id.tabCrash);
+        TabItem tabNetwork = findViewById(R.id.tabNetwork);
+        TabItem tabFlows = findViewById(R.id.tabFlows);
+        TabItem tabOther = findViewById(R.id.tabOther);
     }
 
     public void btnerror (View view)
@@ -58,6 +91,21 @@ public class MainActivity extends AppCompatActivity {
         text.setTextColor(Color.parseColor("#000000"));
         errorbutton.setTextColor(Color.parseColor("#000000"));
 
+    }
+
+    private int count;
+
+
+    public void btniOS (View view)
+    {
+        TextView iOS= findViewById(R.id.counter);
+        iOS.setTextColor(Color.parseColor("#000000"));
+    }
+
+    public void btnandroid (View view)
+    {
+        TextView t1 = findViewById(R.id.title);
+        t1.setTextColor(Color.parseColor("#000000"));
     }
 }
 
